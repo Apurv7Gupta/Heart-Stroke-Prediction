6067aa3e881820da0821ac418c57733c5b38bbef
Apurv Gupta <186622906+Apurv7Gupta@users.noreply.github.com>
2026-03-05 18:54:33 +0000

    Refine Title

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model = joblib.load("Logistic_Regression.pkl")
 scaler = joblib.load("scaler.pkl")
 expected_columns = joblib.load("columns.pkl")
 
-st.title("Heart Stroke Prediction by Apurv Gupta")
+st.title("Heart Stroke Prediction")
 st.markdown("Provide the following details to check your heart stroke risk:")
 
 # Collect user input
